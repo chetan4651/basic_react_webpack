9197056622da943fd5dbd1695ad76553d9e57eb9
Chetan Mohol <chetan.r.mohol@gmail.com>
2024-01-10 15:33:13 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
 import ReactDOM from "react-dom";
 import { App } from "./App";
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// document.getElementById("root").innerHTML = `<h1>Hello Typescript....</h1>`;
