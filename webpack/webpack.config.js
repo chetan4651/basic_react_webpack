6178b7e6fa85b924b881ac579c05a12dc743740d
Chetan Mohol <chetan.r.mohol@gmail.com>
2024-01-10 15:30:00 +0530

    Update webpack.config.js

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    // entry: path.resolve(__dirname, '..', './src/index.tsx'), // load typescript / javascript file
+    // entry: path.resolve(__dirname, '..', './src/index.ts'), // load typescript / javascript file
     entry: path.resolve(__dirname, '..', './src/index.tsx'), // load react component
     resolve: {
         extensions: ['.tsx', '.ts', '.js']
